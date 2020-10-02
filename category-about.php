3a97233f0cf23ac30d97ba4674882aa9f84b9023
Mohamed Sayed <mohamedsayedaz18@gmail.com>
2020-10-02 14:00:03 +0200

    Finish About and Start in Contact Us Page

---
+++ category-about.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+<div class="jumbotron" id="about-section">
+	<div class="a-container center-d">
+			<div class="row">
+		<div class="col-md-6 col-sm-6 col-lg-6 center-d">
+			<h2 class="text-center" id="htitle">About Us</h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12 col-sm-12 col-lg-12">
+			<h3 id="about-f-title">Live Nippon</h3>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12 col-sm-12 col-lg-12">
+			<h3 class="about-title">History</h3>
+			<p class="about-p">Live Nippon Founded 2013 with the intention of helping foreigners Living in Japan, Assisting the tourist to explore Japan more Conveniently, Assisting Japanese Merchandiser to Explore there markets to the expats.</p>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-12 col-sm-12 col-lg-12">
+			<h3 class="about-title">Our Team</h3>
+			<p class="about-p">We are Team of Professional Travel Advisors, IT Engineer, Bloggers, with different Nationalities including Japanese, American, German, Pakistani.</p>
+		</div>
+	</div>
+	</div>
+</div>
+<?php get_footer(); ?>
