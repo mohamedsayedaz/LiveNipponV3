77f41c9f94a188b4287b17f79f82973845803da8
Mohamed Sayed <mohamedsayedaz18@gmail.com>
2020-10-02 17:25:28 +0200

    Finished ContactUs Page

--- category-contact-us.php
+++ category-contact-us.php
@@ -7,13 +7,7 @@
 	</div>
 	<div class="row">
 		<div class="col-md-6 col-sm-6 col-lg-6 center-d" id="contact-form">
-			<form method="POST" action="">
-				<input type="text" name="name" class="form-control contact-txt" placeholder="Enter Your Name">
-				<input type="email" name="email" class="form-control contact-txt" placeholder="Enter Your Email">
-				<input type="text" name="mobile" class="form-control contact-txt" placeholder="Enter Your Mobile Number">
-				<textarea class="form-control contact-txt" placeholder="Enter Your Message"></textarea>
-				<input type="submit" name="sendmail" value="Send" class="send-btn">
-			</form>
+			<?php echo do_shortcode('[contact-form-7 id="12" title="ContactUs"]'); ?>	
 		</div>
 	</div>
 </div>
